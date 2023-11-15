e6211c1268d04a1c23bb44f505e33cb1e6187cb3
VadimKostin <vadkostinwork@gmail.com>
2023-11-15 19:27:36 +0200

    Add general components

--- client/BidMasterOnline/src/app/app.module.ts
+++ client/BidMasterOnline/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AuctionComponent } from './auction/auction.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SearchBarComponent } from './common-components/search-bar/search-bar.component';
+import { DataTableComponent } from './common-components/data-table/data-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuctionComponent,
+    LoginComponent,
+    RegisterComponent,
+    SearchBarComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
