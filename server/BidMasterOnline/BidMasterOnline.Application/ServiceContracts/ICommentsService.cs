895eea7e1593a78ebdee4861f55da8b3e231f953
VadimKostin <vadkostinwork@gmail.com>
2023-12-03 17:51:43 +0200

    Add CommentsController

--- server/BidMasterOnline/BidMasterOnline.Application/ServiceContracts/ICommentsService.cs
+++ server/BidMasterOnline/BidMasterOnline.Application/ServiceContracts/ICommentsService.cs
@@ -12,25 +12,25 @@ namespace BidMasterOnline.Application.ServiceContracts
         /// </summary>
         /// <param name="id">Identifier of the auction to get comments.</param>
         /// <returns>Collection IEnumerable of comments for the specified auction.</returns>
-        Task<IEnumerable<CommentDTO>> GetCommentsByAuctionId(Guid id);
+        Task<IEnumerable<CommentDTO>> GetCommentsForAuctionAsync(Guid id);
 
         /// <summary>
         /// Gets comment by it`s identifier.
         /// </summary>
         /// <param name="id">Identifier of comment to get.</param>
         /// <returns>Comment with passed identifier.</returns>
-        Task<CommentDTO> GetCommentById(Guid id);
+        Task<CommentDTO> GetCommentByIdAsync(Guid id);
 
         /// <summary>
         /// Sets new comment of user for auction.
         /// </summary>
         /// <param name="comment">Comment to set.</param>
-        Task SetNewComment(SetCommentDTO comment);
+        Task SetNewCommentAsync(SetCommentDTO comment);
 
         /// <summary>
         /// Deletes comment with passed identifier.
         /// </summary>
         /// <param name="id">Identifier of comment to delete.</param>
-        Task DeleteComment(Guid id);
+        Task DeleteCommentAsync(Guid id);
     }
 }
