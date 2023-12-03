db37d129e455190c1d1c1dd122da2014e95cf15f
VadimKostin <vadkostinwork@gmail.com>
2023-12-03 16:55:29 +0200

    Add CategoriesController

--- server/BidMasterOnline/BidMasterOnline.API/Program.cs
+++ server/BidMasterOnline/BidMasterOnline.API/Program.cs
@@ -1,11 +1,20 @@
 using BidMasterOnline.Application;
 using BidMasterOnline.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApiVersioning(config =>
+{
+    config.ApiVersionReader = new UrlSegmentApiVersionReader();
+    config.DefaultApiVersion = new ApiVersion(1, 0);
+    config.AssumeDefaultVersionWhenUnspecified = true;
+});
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
---
+++ server/BidMasterOnline/BidMasterOnline.Application/Constants/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace BidMasterOnline.Application.Constants
+{
+    public static class UserRoles
+    {
+        public const string Customer = "Customer";
+        public const string Admin = "Admin";
+    }
+}
